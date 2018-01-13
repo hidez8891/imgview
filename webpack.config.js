e8512bf1e091cdc72a76c7eaf1ac66c44f57f67a
hidez8891 <minowith+github@gmail.com>
2018-01-14 00:01:34 +0900

    リソースのフォルダ変更

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,60 @@
-module.exports = {
-    entry: './resources/app/static/js/index.ts',
-    output: {
-        path: `${__dirname}/output/windows-amd64/resources/app/static/js`,
-        filename: 'index.js'
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const src_dir = "src/resources/app/static";
+const dist_dir = "dist/resources/app/static";
+
+module.exports = [{
+        name: "tsc",
+        entry: `./${src_dir}/js/index.ts`,
+        output: {
+            path: `${__dirname}/${dist_dir}/js`,
+            filename: 'index.js'
+        },
+        module: {
+            rules: [{
+                    test: /\.ts$/,
+                    use: 'awesome-typescript-loader'
+                },
+                {
+                    enforce: 'pre',
+                    test: /\.js$/,
+                    loader: 'source-map-loader'
+                }
+            ]
+        },
+        resolve: {
+            extensions: [
+                '.ts'
+            ]
+        },
+        devtool: 'source-map'
     },
-    module: {
-        rules: [{
-                test: /\.ts$/,
-                use: 'awesome-typescript-loader'
-            },
-            {
-                enforce: 'pre',
-                test: /\.js$/,
-                loader: 'source-map-loader'
-            }
-        ]
-    },
-    resolve: {
-        extensions: [
-            '.ts'
+    {
+        name: "scss",
+        entry: `./${src_dir}/css/index.scss`,
+        output: {
+            path: `${__dirname}/${dist_dir}/css`,
+            filename: 'index.css'
+        },
+        module: {
+            rules: [{
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    use: [{
+                        loader: 'css-loader',
+                        options: {
+                            url: false
+                        }
+                    }, {
+                        loader: 'sass-loader'
+                    }],
+                    fallback: 'style-loader'
+                })
+            }]
+        },
+        plugins: [
+            new ExtractTextPlugin('index.css')
         ],
-        alias: {
-            vue: 'vue/dist/vue.js'
-        }
-    },
-    devtool: 'source-map'
-};
+        devtool: 'source-map'
+    }
+];
